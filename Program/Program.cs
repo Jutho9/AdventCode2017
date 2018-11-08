3dd8c2e5a67c6ed1814bcdad93970e5f318a6229
jtol <jtol@itu.dk>
2018-11-08 14:36:30 +0100

    Console for the program

---
+++ Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Solutions
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Day one solutions
+            // DayOne.Captcha();
+            
+            // Day two solutions
+            Console.WriteLine(DayTwo.Checksum());
+            Console.WriteLine(DayTwo.EvenDiv());
+
+        }
+    }
+}
