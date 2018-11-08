9ce346bf947d817d7173365718beef8f5ab04a25
jtol <jtol@itu.dk>
2018-11-08 14:39:02 +0100

    Added solution for day two

---
+++ DayTwo/DayTwo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Solutions
+{
+    public class DayTwo
+    {
+        // Linq and Lambda
+        private static int[][] array = File.ReadLines(@"/Users/Jutho/Documents/Advent_Programming/Solutions/Solutions/DayTwo/nums.txt")
+            .Select(r => r.Split('\t')  // split by tab
+                .Select(c => Convert.ToInt32(c)).ToArray()) // convert to int and add to array
+                .ToArray(); // add to outer array
+        
+        public static int Checksum()
+        {
+            int sum = 0; // sum of all rows
+            
+            foreach (var row in array)
+            {
+                int min = row.Min(); // min in array
+                int max = row.Max(); // max in array
+                sum += max - min;
+            }
+
+            return sum;
+        }
+
+        public static int EvenDiv()
+        {
+            int sum = 0;
+
+            foreach (var row in array)
+            {
+                for (int i = 0; i < row.Length; i++)
+                {
+                    int j = row.Length - 1; // high
+                    while (j >= 0)
+                    {
+                        if (row[i] % row[j] == 0 && i != j)
+                        {
+                            sum += row[i] / row[j];
+                            Console.WriteLine("row[i]: {0}\n row[j]: {1}\n Sum: {2}", row[i], row[j], sum);
+                        }
+
+                        j--; // move down array
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
